d9c4a00ae507b811736f4bcf7351ad953a5d78ca
Lotte Eliasson <lottefacereliasson@hotmail.com>
2023-09-17 08:15:39 +0200

    ex 5

--- Exercise5.js
+++ Exercise5.js
@@ -5,4 +5,50 @@ const cars = [
     { id: 4, year: 1996, make: 'Jeep', model: 'Grand Cherokee', price: 4799 },
     { id: 5, year: 2005, make: 'Volvo', model: 'V70', price: 44799 }
   ];
+
+
+  //Laver tabellen af biler via id tbody til html
+  function tableCars(cars){
+    const body = document.getElementById('tbody');
+    body.innerHTML = '';
+
+    const rows = cars.map((car) => {
+        return `
+        <tr>
+            <td>${car.id}</td>
+            <td>${car.year}</td>
+            <td>${car.make}</td>
+            <td>${car.model}</td>
+            <td>${car.price}</td>
+        </tr>
+        `;
+
+    });
+    body.innerHTML = rows.join(''); 
+  }
+
+  tableCars(cars);
   
+
+  
+// filtrere cars via eventlistener for en given pris og returnerer en ny filtreret tabel
+const filterBtn = document.getElementById('filterBtn');
+filterBtn.addEventListener('click', () => {
+  const carFilter = document.getElementById('carFilter');
+  const value = parseFloat(carFilter.value);
+
+  if (!isNaN(value)) {
+    const carsByPrice = cars.filter((car) => car.price < value);
+    tableCars(carsByPrice);
+  }
+});
+
+
+
+const clearBtn = document.getElementById('clearFilter'); 
+clearBtn.addEventListener('click', () => {
+    const carFilter = document.getElementById('carFilter'); 
+    carFilter.value = '';
+
+    tableCars(cars);
+});
